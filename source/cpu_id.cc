7670f5aff3b7698b4c044c5b109645d385d09ed8
Sergej Jovanovic <sergej@gnedo.com>
2017-07-12 13:34:36 +0200

    Added RX64 macro check.

--- source/cpu_id.cc
+++ source/cpu_id.cc
@@ -171,7 +171,7 @@ int cpu_info_ = 0;  // cpu_info is not initialized yet.
 
 // Test environment variable for disabling CPU features. Any non-zero value
 // to disable. Zero ignored to make it easy to set the variable on/off.
-#if !defined(__native_client__) && !defined(_M_ARM) && !defined(WINRT)
+#if !defined(__native_client__) && !defined(_M_ARM) && !defined(WINRT) && !defined(RX64)
 
 static LIBYUV_BOOL TestEnv(const char* name) {
   const char* var = getenv(name);
